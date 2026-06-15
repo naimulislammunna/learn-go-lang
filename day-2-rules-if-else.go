38a59fef4dae1807f16df26a38931e73a96623f7
naimulislammunna <naimulislammunna742@gmail.com>
2026-06-15 20:20:30 +0600

    rules and if else

--- day-2-rules-if-else.go
+++ day-2-rules-if-else.go
@@ -33,4 +33,40 @@ func main() {
 	const b = 99
 	fmt.Println(a, b)
 
+	// The brackets in the else statement should be like } else {:
+	age = 39
+
+	if age < 18 {
+		fmt.Println("they are child")
+	} else if age == 18 || age <= 30 {
+		fmt.Println("they are adult")
+	} else if age > 30 {
+		fmt.Println("they are men or women")
+	}
+
+	// switch
+	// 	All the case values should have the same type as the switch expression.
+	// Otherwise, the compiler will raise an error:
+	// It is possible to have multiple values for each case in the switch statement:
+
+	day := 3
+	switch day {
+	case 1:
+		fmt.Println("Monday")
+	case 2:
+		fmt.Println("Tuesday")
+	case 3:
+		fmt.Println("Wednesday")
+	case 4:
+		fmt.Println("Thursday")
+	case 5:
+		fmt.Println("Friday")
+	case 6:
+		fmt.Println("Saturday")
+	case 7:
+		fmt.Println("Sunday")
+	default:
+		fmt.Println("Not a weekday")
+	}
+
 }
