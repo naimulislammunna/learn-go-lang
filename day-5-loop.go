a536b5468044868c464b6793b7e3f7afb3f342a7
naimulislammunna <naimulislammunna742@gmail.com>
2026-06-22 20:20:10 +0600

    loop

--- day-5-loop.go
+++ day-5-loop.go
@@ -26,4 +26,14 @@ func main() {
 		fmt.Println(p)
 	}
 
+	// Nested Loops
+
+	adj := [2]string{"big", "tasty"}
+	fruits := [3]string{"apple", "orange", "banana"}
+	for i := 0; i < len(adj); i++ {
+		for j := 0; j < len(fruits); j++ {
+			fmt.Println(adj[i], fruits[j])
+		}
+	}
+
 }
